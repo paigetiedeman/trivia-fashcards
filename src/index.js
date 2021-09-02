a9929a38a41fd7389141a9233bf1d2d4ba2d73c4
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-02 08:50:32 -0700

    image loader added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import  from './js/scripts.js';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+// import  from './js/scripts.js';
 
-$(document).ready(function){
-
-};
